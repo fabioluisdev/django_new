2d3f3cd86c70c784c29e58655cdcbffbbf164588
fabio1008 <fabiocarini2003@gmail.com>
2022-03-13 12:14:27 -0300

    django toolbar

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Ola Django_New')
+    return HttpResponse('<html><body>Ola Django_New</body></html>', content_type='text/html')
