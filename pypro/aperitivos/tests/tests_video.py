37b6bee07d5d44e7811078a329200462b8a9984b
fabio1008 <fabiocarini2003@gmail.com>
2022-03-17 19:36:02 -0300

    Video com Slug | Filtro | Elementos for.

--- pypro/aperitivos/tests/tests_video.py
+++ pypro/aperitivos/tests/tests_video.py
@@ -14,8 +14,8 @@ def test_status_code(resp):
 
 
 def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Video Aperitivo: Motivacao</h1>')
+    assert_contains(resp, 'Video Aperitivo: Motivacao')
 
 
 def test_conteudo_video(resp):
-    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/688843510?h=e6cdb1e49f"')
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/688843510"')
