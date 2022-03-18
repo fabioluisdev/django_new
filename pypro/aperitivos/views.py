787c6833800276cf16c592349dd1f3c41d8faf29
fabio1008 <fabiocarini2003@gmail.com>
2022-03-17 23:50:35 -0300

    Identacao link_videos

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -24,18 +24,6 @@ def indice(request):
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
-videos = [
-        {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivacao', 'vimeo_id': 688843510},
-        {'slug': 'instalacao-windows', 'titulo': 'Instalacao Windows', 'vimeo_id': 251497668},
-]
-
-videos_dct = {dct['slug']: dct for dct in videos}
-
-
-def indice(request):
-    return render(request, 'aperitivos/indice.html', context={'videos': videos})
-
-
 def video(request, slug):
     video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
