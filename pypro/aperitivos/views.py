9dfd0a53be885d87b3448b4e173a0052c45ac5fd
fabio1008 <fabiocarini2003@gmail.com>
2022-03-18 23:43:18 -0300

    Introducao Migrations.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,20 +1,10 @@
 from django.shortcuts import render
-from django.urls import reverse
 
-
-class Video:
-    def __init__(self, slug, titulo, vimeo_id):
-        self.slug = slug
-        self.titulo = titulo
-        self.vimeo_id = vimeo_id
-
-    def get_absolute_url(self):
-        return reverse('aperitivos:video', args=(self.slug,))
-
+from pypro.aperitivos.models import Video
 
 videos = [
-    Video('motivacao', 'Video Aperitivo: Motivacao', 688843510),
-    Video('instalacao-windows', 'Instalacao Windows', 251497668),
+    Video(slug='motivacao', titulo='Video Aperitivo: Motivacao', vimeo_id=688843510),
+    Video(slug='instalacao-windows', titulo='Instalacao Windows', vimeo_id=251497668),
 ]
 
 videos_dct = {v.slug: v for v in videos}
@@ -25,5 +15,5 @@ def indice(request):
 
 
 def video(request, slug):
-    video = videos_dct[slug]
+    video = Video.objects.get(slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
