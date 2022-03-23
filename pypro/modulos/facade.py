5c24d30be0c574b9ab95ee88a1cea14b0e81e844
fabio1008 <fabiocarini2003@gmail.com>
2022-03-22 23:06:26 -0300

    Modulos. Migracoes. Dropdown. Apps Terceiros.

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -10,3 +10,7 @@ def listar_modulos_ordenados() -> List[Modulo]:
     """
 
     return list(Modulo.objects.order_by('titulo').all())
+
+
+def encontrar_modulo(slug: str) -> Modulo:
+    return Modulo.objects.get(slug=slug)
