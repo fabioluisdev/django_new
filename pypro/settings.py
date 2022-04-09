eb05ff97ac750439c107627c24951413835bf44a
fabio1008 <fabiocarini2003@gmail.com>
2022-04-08 21:28:27 -0300

    Update botocore from 1.24.28 to 1.24.29

--- pypro/settings.py
+++ pypro/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 
 # Configuracao de envio e Email
 
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_BACKEND = config('EMAIL_BACKEND')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 # Configuracao Django Debug Tollbar
 INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
